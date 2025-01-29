c0419af615d117afad75e6e0713c6a75f847efb7
Alexander Zerpa <a.zerpa@zaleos.net>
2025-01-29 10:59:26 +0100

    feat(internal.commands): implement list flags

--- internal/commands/list.go
+++ internal/commands/list.go
@@ -6,7 +6,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var All bool
+var Done bool
+
 func init() {
+	list.Flags().BoolVarP(&All, "all", "", false, "list all todos")
+	list.Flags().BoolVarP(&Done, "done", "", false, "list done todos")
 	rootCmd.AddCommand(list)
 }
 
@@ -14,6 +19,12 @@ var list = &cobra.Command{
 	Use:   "list",
 	Short: "list saved todos",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("stuff to do")
+		if All {
+			fmt.Println("list all things")
+		} else if Done {
+			fmt.Println("list done things")
+		} else {
+			fmt.Println("list things")
+		}
 	},
 }
